1f2abd6f6dd430656dd126209d1bb97b0a69f143
19mariusz95 <19mariusz95@gmail.com>
2016-03-25 11:02:33 +0100

    zad1 looks working

--- zad1/main.c
+++ zad1/main.c
@@ -1,22 +1,58 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <math.h>
 #include <signal.h>
+#include <unistd.h>
 
 #define ala 3
 
-void print_poem(FILE *file);
+char *path;
 
-void print_poem_backwards(FILE *file);
+int num = 1;
+
+int delta = 1;
+
+int tmp = 1;
+
+int backwards = 1;
+
+int max_num;
+
+void print_poem();
+
+void print_poem_backwards();
+
+FILE *get_file();
 
 void sighandler(int signum) {
     switch (signum) {
         case SIGINT: {
-            printf("Odebrano sygnał SIGINT");
+            printf("Odebrano sygnał SIGINT\n");
             exit(0);
         }
         case SIGTSTP: {
-
+            printf("Odebrano sygnał SIGTSTP\n");
+            if (backwards == 0) {
+                for (int i = 0; i < num; i++)
+                    print_poem();
+            } else {
+                for (int i = 0; i < num; i++)
+                    print_poem_backwards();
+            }
+            if (tmp == 0) {
+                backwards = 1;
+            } else if (tmp == 1) {
+                backwards = 0;
+            } else {
+                num += delta;
+                if (num > max_num) {
+                    delta = -1;
+                    num -= 2;
+                } else if (num == 0) {
+                    delta = 1;
+                    num += 2;
+                }
+            }
+            tmp = (tmp + 1) % 3;
             break;
         }
         default:
@@ -25,35 +61,27 @@ void sighandler(int signum) {
     return;
 }
 
-int num = 1;
-
-int backwards = 0;
-
-int max_num;
-
 int main(int argc, char *argv[]) {
     if (argc < 3) {
         printf("Trolololo");
         exit(1);
     }
-    char *path = argv[1];
+    path = argv[1];
     max_num = atoi(argv[2]);
     if (max_num == 0) {
         printf("trolololo");
         exit(1);
     }
-
-    FILE *file = fopen(path, "r+");
-    if (file == NULL) {
-        printf("Trololo");
-        exit(1);
-    }
-    signal(SIGINT, sighandler);
-
-    return 0;
+    print_poem();
+    do {
+        signal(SIGINT, sighandler);
+        signal(SIGTSTP, sighandler);
+        pause();
+    } while (1);
 }
 
-void print_poem_backwards(FILE *file) {
+void print_poem_backwards() {
+    FILE *file = get_file();
     char alamakota[ala + 1];
     long tmp;
     fseek(file, 0, SEEK_END);
@@ -74,14 +102,27 @@ void print_poem_backwards(FILE *file) {
         alamakota[tmp] = '\0';
         fseek(file, -tmp, SEEK_CUR);
     }
-
+    printf("\n");
+    fclose(file);
 }
 
-void print_poem(FILE *file) {
+FILE *get_file() {
+    FILE *file = fopen(path, "r+");
+    if (file == NULL) {
+        printf("Trololo");
+        exit(1);
+    }
+    return file;
+}
+
+void print_poem() {
+    FILE *file = get_file();
     char alamakota[ala + 1];
     long tmp;
     while ((tmp = fread(&alamakota, 1, ala, file)) > 0) {
         alamakota[tmp] = '\0';
         printf(alamakota);
     }
+    printf("\n");
+    fclose(file);
 }
